4eaee68d877bf9f894e705da9d24d3e202ab4aa5
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-25 17:33:49 -0500

    add select days component which dynamically renders days from application

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import DayListItem from "components/DayListItem";
 
-export default function dayList(props) {
+export default function DayList(props) {
   const days = props.days.map(day => {
     return (
       <DayListItem
