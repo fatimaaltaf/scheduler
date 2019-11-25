76b199b42c614d463e7b43f50d2defd42a832d6d
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-25 15:07:10 -0500

    DayListItem test pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,8 +3,15 @@ import React from "react";
 import classnames from "classnames";
 
 import "components/DayListItem.scss";
+import { tsParameterProperty } from "@babel/types";
 
 export default function DayListItem(props) {
+
+  const formatSpots = (spots) => {
+    if (spots === 0) return "no spots remaining";
+    else if (spots === 1) return "1 spot remaining";
+    else return `${spots} spots remaining`;
+  };
 
   const dayClass = classnames("day-list", "day-list__item", {
     "day-list__item--selected": props.selected === true,
@@ -14,7 +21,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
