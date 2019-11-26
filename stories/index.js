db6c45517c47d9e34858a5851e7e6026d3243885
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-26 17:17:31 -0500

    add display appointments component which shows header with time

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -147,6 +147,22 @@ storiesOf("InterviewerList", module)
     })
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="12pm" />
+        <Appointment id="last" time="1pm" />
+      </Fragment>
+    ))
+    .add("Appointment Booked", () => (
+      <Fragment>
+        <Appointment
+          id={1}
+          time="12pm"
+          interview={{ student: "Lydia Miller-Jones", interviewer }}
+        />
+        <Appointment id="last" time="1pm" />
+      </Fragment>
+    ))
     .add("Header", () => <Header time="12pm" />)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
     .add("Show", () => (
@@ -171,6 +187,13 @@ storiesOf("InterviewerList", module)
       onClose={action("onClose")}
       />
     ))
+    .add("Create", () => (
+      <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+      />
+    ))
     .add("Edit", () => (
     <Form 
       name={"Miss Terry"}
@@ -180,10 +203,4 @@ storiesOf("InterviewerList", module)
       onCancel={action("onCancel")}
     />
     ))
-    .add("Create", () => (
-      <Form 
-      interviewers={interviewers}
-      onSave={action("onSave")}
-      onCancel={action("onCancel")}
-      />
-    ))
+   
