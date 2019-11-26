8d4f3f47178dde486a3b3b26007235ebf9dca1cd
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-26 16:40:00 -0500

    add form component and render form that saves and cancels (clears form)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,27 +3,42 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel()
+  }
+
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+    
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
-            /*
-              This must be a controlled component
-            */
+            value={name}
+            onChange={event => setName(event.target.value)}
           />
         </form>
-    <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+    <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger onClick={props.onCancel}>Cancel</Button>
-      <Button confirm onClick={props.onSave}>Save</Button>
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onClick={save}>Save</Button>
     </section>
   </section>
 </main>
