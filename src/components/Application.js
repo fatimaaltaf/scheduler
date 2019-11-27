3f90c1a0c1e4db598e56f5922b33dfe5b7a59258
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-27 15:38:34 -0500

    manage state by creating state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -59,8 +59,19 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}));
+
+
+  // const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:8001/api/days`).then((response) => {
@@ -88,8 +99,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
