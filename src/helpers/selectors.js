5eb2d0a282e1a4bdb410f2063493ee8bb9b652a9
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-27 14:58:44 -0500

    add getAppointmentsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,17 @@
-// const getAppointmentsForDay = (state, day) => {
-//   const filteredDay = state.days.filter( => user.name === name);
-//   return filteredDay;
-// };
+export function getAppointmentsForDay(state, day) {
+  const filtered = state.days.filter(d => d.name === day)
+
+  if (filtered[0] === undefined) {
+    return [];
+  }
+  const filteredAppointments = filtered[0].appointments;
+
+  const result = [];
+
+  for (let appointment in state.appointments) {
+    if (filteredAppointments.includes(state.appointments[appointment].id)) {
+      result.push(state.appointments[appointment])
+    }
+  }
+  return result;
+}
