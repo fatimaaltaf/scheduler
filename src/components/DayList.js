895b2c8c12772eecac15e3bd8b35857d389f43f8
Fatima Altaf <fatima.altaf@gmail.com>
2019-11-28 15:53:56 -0500

    selectors test files update and new function to find interviewer by day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,6 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  console.log("props is: ", props);
   const days = props.days.map(day => {
     return (
       <DayListItem
