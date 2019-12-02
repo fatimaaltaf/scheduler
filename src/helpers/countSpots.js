7ad56f29891f26a0e9c7be04bca1ba87ee9fd684
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-02 10:32:16 -0500

    edits

--- src/helpers/countSpots.js
+++ src/helpers/countSpots.js
@@ -5,7 +5,7 @@ export default function countSpots(state, days) {
   let resultArr = [];
   for (let day of days) {
     resultArr.push(getAppointmentsForDay(state, day.name)
-    .map(element => element.interview !== null ? 0 : 1)
+    .map(i => i.interview !== null ? 0 : 1)
     .reduce((tally, current) => tally + current)
   )}
   return resultArr;
