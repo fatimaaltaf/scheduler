0b5d2c898d247e4ec55fa9afc549778f32fdcba3
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-02 20:16:38 -0500

    application component given async nature

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,12 +6,23 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-
-  await waitForElement(() => getByText("Monday"));
-
-  fireEvent.click(getByText("Tuesday"));
-
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { getByText } = render(<Application />)
+  
+    await waitForElement(() => getByText("Archie Cohen"))
+  
+    fireEvent.click()
+  })
+  
+})
