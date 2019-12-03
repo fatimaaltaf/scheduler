a06b0eaa7511febffd01201194c7650bab604fe1
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-03 18:00:11 -0500

    create reducers file for reducer function and remove from useAppData file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,14 @@
 import React, { useReducer, useEffect } from "react";
 import axios from 'axios';
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
+// const SET_DAY = "SET_DAY";
+// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+// const SET_INTERVIEW = "SET_INTERVIEW";
 
 const stateInitial = {
   day: "Monday",
@@ -12,45 +17,45 @@ const stateInitial = {
   interviewers: {}
 };
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case SET_DAY:
-        return {...state, day: action.day}
-      case SET_APPLICATION_DATA:
-        return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers}
-        case SET_INTERVIEW: {
-          const { id, interview } = action;
-          return {
-            ...state,
-            days: state.days.map((day) => {
-              let spotCounter = 0;
-              if(day.name === state.day) {
-                if (interview && state.appointments[id].interview) {
-                  spotCounter = 0;
-                } else if(interview) {
-                  spotCounter = -1;
-                } else {
-                  spotCounter = 1;
-                }
-              } 
-              return {...day,
-                      spots: day.spots + spotCounter};
-            }),
-            appointments: {
-              ...state.appointments,
-              [id]: {
-                ...state.appointments[action.id],
-                interview: action.interview ? {...interview} : null
-              } 
-            }
-          }
-        }  
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  }
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case SET_DAY:
+//       return {...state, day: action.day}
+//     case SET_APPLICATION_DATA:
+//       return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers}
+//       case SET_INTERVIEW: {
+//         const { id, interview } = action;
+//         return {
+//           ...state,
+//           days: state.days.map((day) => {
+//             let spotCounter = 0;
+//             if(day.name === state.day) {
+//               if (interview && state.appointments[id].interview) {
+//                 spotCounter = 0;
+//               } else if(interview) {
+//                 spotCounter = -1;
+//               } else {
+//                 spotCounter = 1;
+//               }
+//             } 
+//             return {...day,
+//                     spots: day.spots + spotCounter};
+//           }),
+//           appointments: {
+//             ...state.appointments,
+//             [id]: {
+//               ...state.appointments[action.id],
+//               interview: action.interview ? {...interview} : null
+//             } 
+//           }
+//         }
+//       }  
+//     default:
+//       throw new Error(
+//         `Tried to reduce with unsupported action type: ${action.type}`
+//       );
+//   }
+// }
 
 export default function useApplicationData() {
 
