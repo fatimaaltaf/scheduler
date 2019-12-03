f56116aaba8bc52ebf112ecb5102f8c83ee96ea1
Fatima Altaf <fatima.altaf@gmail.com>
2019-12-03 18:12:17 -0500

    delete unused code from Application Data file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,10 +6,6 @@ import reducer, {
   SET_INTERVIEW
 } from "reducers/application";
 
-// const SET_DAY = "SET_DAY";
-// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-// const SET_INTERVIEW = "SET_INTERVIEW";
-
 const stateInitial = {
   day: "Monday",
   days: [],
@@ -17,45 +13,6 @@ const stateInitial = {
   interviewers: {}
 };
 
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case SET_DAY:
-//       return {...state, day: action.day}
-//     case SET_APPLICATION_DATA:
-//       return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers}
-//       case SET_INTERVIEW: {
-//         const { id, interview } = action;
-//         return {
-//           ...state,
-//           days: state.days.map((day) => {
-//             let spotCounter = 0;
-//             if(day.name === state.day) {
-//               if (interview && state.appointments[id].interview) {
-//                 spotCounter = 0;
-//               } else if(interview) {
-//                 spotCounter = -1;
-//               } else {
-//                 spotCounter = 1;
-//               }
-//             } 
-//             return {...day,
-//                     spots: day.spots + spotCounter};
-//           }),
-//           appointments: {
-//             ...state.appointments,
-//             [id]: {
-//               ...state.appointments[action.id],
-//               interview: action.interview ? {...interview} : null
-//             } 
-//           }
-//         }
-//       }  
-//     default:
-//       throw new Error(
-//         `Tried to reduce with unsupported action type: ${action.type}`
-//       );
-//   }
-// }
 
 export default function useApplicationData() {
 
